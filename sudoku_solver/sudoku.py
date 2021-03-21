934b42916b3c759eed74c7ee4b072aaeca36300c
Andreas Rabenstein <andreas.rabenstein@physik.uni-regensburg.de>
2021-03-21 20:43:50 +0100

    enh: Reshape internal board to 1d numpy array

--- sudoku_solver/sudoku.py
+++ sudoku_solver/sudoku.py
@@ -19,6 +19,8 @@ def highlight_string(text: str, style: str = "OKGREEN") -> str:
 
 
 class Sudoku:
+    _set_directions = ["row", "col", "quadrant"]
+
     def __init__(self, board: list):
         """Iniitialize board
 
@@ -32,34 +34,63 @@ class Sudoku:
         ValueError
             Raised if the the array is not square
         """
-        self.initial_board = board
-        self.current_board = board
-        self.size = len(self.initial_board)
+
+        self.size = len(board)
         self.field_size = int(np.sqrt(self.size))
         if self.field_size ** 2 != self.size:
             raise ValueError("Something is wrong with the input dimension")
-        for row in self.initial_board:
+        for row in board:
             if len(row) != self.size:
                 raise ValueError("Not all rows have the same length.")
+        #
+        self.initial_board = np.hstack(board)
+        self.current_board = np.hstack(board)
+
+    def get_indices(self, pos):
+        idx_row = pos // self.size
+        idx_col = pos % self.size
+        return idx_row, idx_col
+
+    def get_position(self, idx_row, idx_col):
+        return idx_row * self.size + idx_col
 
     def pretty_print(self, highlight=[[]]):
         vline = "-" * (2 * self.size + 2 * self.field_size + 1)
         string = vline + "\n"
-        for row_cnt, row in enumerate(self.current_board):
+        for idx_row in range(self.size):
             string += "|"
-            for col_cnt, col in enumerate(row):
-                val = str(col)
-                if [row_cnt, col_cnt] in highlight:
-                    print("INSIDE")
+            for idx_col in range(self.size):
+                position = self.get_position(idx_row, idx_col)
+                val = self.current_board[position]
+                val = str(val) if val != 0 else "_"
+                if [idx_row, idx_col] in highlight:
                     val = highlight_string(val, style="FAIL")
                 string += " " + val
-                if col_cnt % self.field_size == 2:
+                if idx_col % self.field_size == 2:
                     string += " |"
-            if row_cnt % self.field_size == 2:
+            if idx_row % self.field_size == 2:
                 string += "\n" + vline
             string += "\n"
         return string
 
     def do_solve_step(self) -> bool:
+        # 1. Look for unique possiblities
+        # 2. Go over rows
+        # 3. Go over columns
+        # 4. Go over quadrants
+        # 5. Do tryout
         logging.debug("")
+
+    def check_matching(self, field_cnt: int, direction: str = "row"):
+        if direction not in self._set_directions:
+            raise ValueError(
+                "direction has to be in [" + ", ".join(self._set_directions)
+            )
+        if field_cnt >= self.size:
+            raise ValueError(
+                "The field count has to be in [0," + str(self.size - 1) + "]"
+            )
+
+    def get_members(self, field_cnt: int, direction: str = "row") -> list:
+        pass
 
--- tests/test_sudoku_class.py
+++ tests/test_sudoku_class.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from sudoku_solver.sudoku import Sudoku
 
@@ -29,8 +30,8 @@ class TestClassSudoku:
 
     def test_init(self):
         x = Sudoku(self.start_board)
-        assert x.initial_board == self.start_board
-        assert x.current_board == self.start_board
+        np.testing.assert_array_equal(x.initial_board, np.hstack(self.start_board))
+        np.testing.assert_array_equal(x.current_board, np.hstack(self.start_board))
         assert x.size == self.size
         assert x.field_size == self.field_size
 
@@ -46,17 +47,24 @@ class TestClassSudoku:
         x = Sudoku(self.start_board)
         expected_string = (
             "-------------------------\n"
-            + "| 2 0 6 | 0 0 0 | 0 0 8 |\n"
-            + "| 0 0 \x1b[91m0\x1b[0m | 0 0 0 | 6 7 0 |\n"
-            + "| 7 8 9 | 1 0 6 | 2 0 0 |\n"
+            + "| 2 _ 6 | _ _ _ | _ _ 8 |\n"
+            + "| _ _ \x1b[91m_\x1b[0m | _ _ _ | 6 7 _ |\n"
+            + "| 7 8 9 | 1 _ 6 | 2 _ _ |\n"
             + "-------------------------\n"
-            + "| 3 1 0 | 0 6 5 | 0 0 0 |\n"
-            + "| 0 0 0 | 0 9 1 | 0 2 0 |\n"
-            + "| 0 9 0 | 0 2 0 | 0 4 0 |\n"
+            + "| 3 1 _ | _ 6 5 | _ _ _ |\n"
+            + "| _ _ _ | _ 9 1 | _ 2 _ |\n"
+            + "| _ 9 _ | _ 2 _ | _ 4 _ |\n"
             + "-------------------------\n"
-            + "| 0 0 1 | 6 0 0 | 0 8 3 |\n"
-            + "| 8 6 4 | 0 1 0 | 0 5 0 |\n"
-            + "| 0 0 3 | 5 8 2 | 4 6 1 |\n"
+            + "| _ _ 1 | 6 _ _ | _ 8 3 |\n"
+            + "| 8 6 4 | _ 1 _ | _ 5 _ |\n"
+            + "| _ _ 3 | 5 8 2 | 4 6 1 |\n"
             + "-------------------------\n"
         )
         assert x.pretty_print(highlight=[[1, 2]]) == expected_string
+
+    def test_check_matching(self):
+        x = Sudoku(self.start_board)
+        with pytest.raises(ValueError):
+            x.check_matching(0, direction="head")
+        with pytest.raises(ValueError):
+            x.check_matching(110, direction="row")
